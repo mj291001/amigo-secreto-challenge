8df5260d78c21a3229e2a48dd8041ea16b217c46
Majo Ceballos <mariajoseceballos45@gmail.com>
2025-01-30 18:12:10 -0600

    Actualización app

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+//Array para nombres
+let amigos = [];
+
+//Función para agregar amigos
+function agregarAmigo () {
+    let nombreDeAmigo = document.getElementById("amigo").value;
+     
+
+}
