fd6f3b91334c1366ed303bac14b3d447f3432b37
Majo Ceballos <mariajoseceballos45@gmail.com>
2025-02-04 18:10:20 -0600

    Agregar nombres

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
 //Array para nombres
-let amigos = [];
+const amigos = [];
+
 
 //Función para agregar amigos
 function agregarAmigo () {
-    let nombreDeAmigo = document.getElementById("amigo").value;
+    const nombreDeAmigo = document.getElementById("amigo").value;
      
-
+    if (nombreDeAmigo === "") {
+        alert ("Por favor, inserte un nombre");
+    } else {
+        amigos.push(nombreDeAmigo);
+        mostrarNombres(nombreDeAmigo);
+        limpiarInput();
+    }
 }
+
+function mostrarNombres(nombre) {
+    const listaDeAmigos = document.getElementById("listaAmigos");
+    const nuevoItem = document.createElement("li")
+    nuevoItem.textContent = nombre
+    listaDeAmigos.appendChild(nuevoItem)
+}
+
+function limpiarInput () {
+    document.getElementById("amigo").value="";
+}
+
